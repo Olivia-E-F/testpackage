cfe4a1a1ac9b2cafbfc66bf4572c8d6b98945cc6
Olivia Freides <oliviafreides@gmail.com>
2021-02-19 10:58:00 -0500

    Added help page

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,12 @@
+#' Concatenate levels of two Factors
+#'
+#' @param factor
+#' @param factor
+#'
+#' @return
+#' @export
+#'
+#' @examples
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
